8d9ca967c161d6fe99fb9a58e1bc88e2daa8f621
egozverev <zzwerruga@mail.ru>
2019-03-17 16:28:40 +0300

    Rebuilding architecture. Remove lots of .txt files with one .json config file. And some minor changes

--- interface.py
+++ interface.py
@@ -5,31 +5,24 @@ def creative_print(str):
     print('-' * 45, "\\ (•◡•) /" * 5, str, '-' * 45, sep='\n', end='\n')
 
 
-def show_interface(path, status):
+def show_interface(config, name):
     '''
     Show location info , status and list of options
     :param path: Path to the location
     :return: Nothing, but print all the input parameters in cool way using CreativePrint()
     '''
-    description = path + 'description.txt'
-    options = path + 'options.txt'
+    description = config["loc_descriptions"][name]
+    options = config["loc_options"][name]
 
     creative_print('LOCATION DESCRIPTION')
-    with open(description) as file:
-        for line in file:
-            print(line, end='')
-        print('\n')
+    print(description)
     creative_print('CHARACTER STATUS')
-    for field, value in status.items():
+    for field, value in config["status"].items():
         print(field, ':: ', value)
     creative_print('OPTIONS')
-    with open(options) as file:
-        i = 1
-        for line in file:
-            print(i, ': ', line, end='')
-            i += 1
-        if i==1:
-            print('Seems we don`t have options here. Probably it is a final scene?\nYou know, in this case you don`t need options.')
-        print('\n')
+    for i, line in enumerate(config["loc_options"][name]):
+        print(i + 1, ': ', line)
+    if (not len(config["loc_options"][name])):
+        print(
+            'Seems we don`t have options here. Probably it is a final scene?\nYou know, in this case you don`t need options.')
     print('-' * 45, '-' * 45, sep='\n')
-
--- main.py
+++ main.py
@@ -1,111 +1,79 @@
+# -*- coding: UTF-8 -*-
 import interface
 import sys
+import json
+import argparse
 
 
-def check_state(path, status):
-    '''
-    Check if character`s status matches the state
-    :param path: str, path to the state which has to be checked
-    :param status: dict, contains current character status
-    :return: True if status matсhes requirements, false otherwise.
-    '''
-    with open(path) as file:
-        for line in file:
-            line = line.split()
-            key = line[0]
-            operator = line[1]
-            value = line[2]
-            if value.isdigit():
-                value = int(value)
-            if key not in status:
-                return False
-            if operator == 'is' and not status[key] == value:
-                return False
-            if operator == 'less' and not status[key] < value:
-                return False
-            if operator == 'more' and not status[key] > value:
-                return False
+def check_state(config, state):
+    for line in config["states"][state]:
+        line = line.split()
+        key = line[0]
+        operator = line[1]
+        value = line[2]
+        if value.isdigit():
+            value = int(value)
+        if key not in config["status"]:
+            return False
+        if operator == 'is' and not config["status"][key] == value:
+            return False
+        if operator == 'less' and not config["status"][key] < value:
+            return False
+        if operator == 'more' and not config["status"][key] > value:
+            return False
     return True
 
 
-def change_state(path, status):
-    '''
-    Change character`s status
-    :param path: str, path to the state which has to be checked
-    :param status: dict, contains current character status
-    :return: nothing
-    '''
-    with open(path) as file:
-        for line in file:
-            line = line.split()
-            key = line[0]
-            operator = line[1]
-            value = line[2]
-            if value.isdigit():
-                value = int(value)
-            if key not in status or operator == 'make':
-                status[key] = value
-            if operator == '+':
-                status[key] += value
-            if operator == '-':
-                status[key] -= value
+def change_state(config, change):
+    for line in config["state_changes"][change]:
+        line = line.split()
+        key = line[0]
+        operator = line[1]
+        value = line[2]
+        if value.isdigit():
+            value = int(value)
+        if key not in config["status"] or operator == 'make':
+            config["status"][key] = value
+        if operator == '+':
+            config["status"][key] += value
+        if operator == '-':
+            config["status"][key] -= value
 
 
-def play_location(path, name, status):
+def play_location(config, name):
     '''
     Does everyhing is necessary to interact with the new location.
-    :param path: str, path to the scenario ( MENTION: not the location name )
+    :param config: dict, configuration parameters of scenario
     :param name: str, name of the location
-    :param status: dict, contains current character status
     :return:
     '''
-    interface.show_interface(path + 'locations/' + name + '/', status)
-    number_of_choices = sum(1 for _ in open(path + 'locations/' + name + '/options.txt'))
+    interface.show_interface(config, name)
+    number_of_choices = len(config["loc_options"][name])
     if not number_of_choices:
         return
     while True:
         choice = int(input())
         if 1 <= choice <= number_of_choices:
-            required = str()
-            with open(path + 'locations/' + name + '/required.txt') as file:
-                for i, line in enumerate(file):
-                    if i + 1 == choice:
-                        required = line.strip()
+            required = config["loc_required"][name][choice - 1]
             if required != 'nothing':
-                if not check_state(path + 'states/' + required, status):
+                if not check_state(config, required):
                     print("It is not possible to do it. (>﹏<) ")
                     continue
 
-            change = str()
-            with open(path + 'locations/' + name + '/changes.txt') as file:
-                for i, line in enumerate(file):
-                    if i + 1 == choice:
-                        change = line.strip()
+            change = config["loc_changes"][name][choice - 1]
             if (change != 'nothing'):
-                change_state(path + 'state_changes/' + change, status)
+                change_state(config, change)
             stop_flag = False  # True if we don`t need to go to location
-            with open(path + 'basic_check.txt') as file:
-                for line in file:
-                    line = line.split()
-                    condition = line[0]
-                    location = line[1].strip()
-                    if check_state(path + 'states/' + condition, status) and not stop_flag:
-                        stop_flag = True
-                        play_location(path, location, status)
+            message = config["loc_messages"][name][choice - 1]
+            print('|' * 45, message, '|' * 45, sep='\n')
+            for condition, location in config["basic_check"].items():
+                if check_state(config, condition) and not stop_flag:
+                    stop_flag = True
+                    play_location(config, location)
             if stop_flag:
                 return
-            location = str()
-            with open(path + 'locations/' + name + '/next_locations.txt') as file:
-                for i, line in enumerate(file):
-                    if i + 1 == choice:
-                        location = line.strip()
-            message = str()
-            with open(path + 'locations/' + name + '/messages.txt') as file:
-                for i, line in enumerate(file):
-                    if i + 1 == choice:
-                        message = line.strip()
-            print('|' * 45, message, '|' * 45, sep='\n')
-            play_location(path, location, status)
+            location = config["loc_next"][name][choice - 1]
+            play_location(config, location)
             break
         print('(>_<) Paaardon?? There is no such an option. (>_<)')
 
@@ -116,16 +84,14 @@ def play_scenario(path):
     :param path: path to the scenario
     :return:
     '''
-    status = dict()
-    with open(path + 'default_status.txt') as file:
-        for line in file:
-            line = line.split()
-            if line[1].isdigit():
-                line[1] = int(line[1])
-            status[line[0]] = line[1]
-    play_location(path, 'start', status)
+    config = dict()
+    with open(path, "r", encoding="utf-8") as load_file:
+        config = json.load(load_file)
+    play_location(config, "start")
 
 
 if __name__ == '__main__':
-    scenario_dir = sys.argv[1]
-    play_scenario(scenario_dir)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('scenario_dir')
+    args = parser.parse_args()
+    play_scenario(args.scenario_dir)
